700f8f789abc70a5f0d1cb9db49e976276767a87
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-08-16 18:06:16 +0600

    some issue fixed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,25 @@
-import Card from "./Card";
+// import Card from "./Card";
 
-const Home = () => {
-    return (
-        <div className="lg:mt-24 mt-20 mx-auto container">
-            <div 
-                className="bg-cover bg-center h-screen relative" 
-                style={{ backgroundImage: "url('https://i.ibb.co/HVyRkqy/download-2.jpg')" }}
-            >
+// const Home = () => {
+//     return (
+//         <div className="lg:mt-24 mt-20 mx-auto container">
+//             <div 
+//                 className="bg-cover bg-center h-screen relative" 
+//                 style={{ backgroundImage: "url('https://i.ibb.co/HVyRkqy/download-2.jpg')" }}
+//             >
                 
 
-            </div>
-            <Card />
+//             </div>
+//             <Card />
 
-            <div className="bg-cover  h-screen bg-center" style={{backgroundImage: "url('https://i.ibb.co/2v67gMT/Capture.png')"}} >
+//             <div className="bg-cover  h-screen bg-center" style={{backgroundImage: "url('https://i.ibb.co/2v67gMT/Capture.png')"}} >
 
-           <h1 className="text-red-500  absolute  ml-40 mb-3  border-2 border-red-500">Hello</h1>
-            </div>
+//            <h1 className="text-red-500  absolute  ml-40 mb-3  border-2 border-red-500">Hello</h1>
+//             </div>
 
             
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default Home;
+// export default Home;
